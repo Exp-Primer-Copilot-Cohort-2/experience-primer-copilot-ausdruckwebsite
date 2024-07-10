d1d9381aa09f67813aa6b1d252a7f0a065a31981
Julius Röderich <167768912+ausdruckwebsite@users.noreply.github.com>
2024-07-10 23:09:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,22 @@
 // create a web server
-// create a web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var path = require('path');
-var comments = require('./comments.json');
-var _ = require('underscore');
-var request = require('request');
-var cheerio = require('cheerio');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// import the comments data
+const comments = require('./data/comments');
 
-app.get('/comments', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
-});
+// import the posts data
+const posts = require('./data/posts');
 
-app.post('/comments', function(req, res) {
-  comments.push(req.body);
-  fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(comments));
-  });
-});
+// import the users data
+const users = require('./data/users');
 
-app.delete('/comments', function(req, res) {
-  comments = [];
-  fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(comments));
-  });
-});
+// import the likes data
+const likes = require('./data/likes');
 
-app.get('/scrape', function
+// import the cors package
+const cors = require('cors');
+
+// use the cors package
+app.use(cors());
