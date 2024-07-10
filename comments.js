676e2b1be4e4ca71e95c76bfac6f699a069e57a2
Julius Röderich <167768912+ausdruckwebsite@users.noreply.github.com>
2024-07-10 23:31:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,14 @@
-// create a web server
+// create web server
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// import the comments data
-const comments = require('./data/comments');
+// create a route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-// import the posts data
-const posts = require('./data/posts');
-
-// import the users data
-const users = require('./data/users');
-
-// import the likes data
-const likes = require('./data/likes');
-
-// import the cors package
-const cors = require('cors');
-
-// use the cors package
-app.use(cors());
+// start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
