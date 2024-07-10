53d132549d5aac23fe32630476d2bc3dd4ff83aa
Julius Röderich <167768912+ausdruckwebsite@users.noreply.github.com>
2024-07-10 23:07:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,20 +1,38 @@
-// create web server
-// create a server
-const express = require('express');
-const app = express();
+// create a web server
+// create a web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var comments = require('./comments.json');
+var _ = require('underscore');
+var request = require('request');
+var cheerio = require('cheerio');
 
-// create a router
-const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// create a route
-router.get('/comments', (req, res) => {
-  res.send('This is the comments page');
+app.get('/comments', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
 });
 
-// mount the router
-app.use('/app', router);
-
-// start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.post('/comments', function(req, res) {
+  comments.push(req.body);
+  fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(comments));
+  });
 });
+
+app.delete('/comments', function(req, res) {
+  comments = [];
+  fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(comments));
+  });
+});
+
+app.get('/scrape', function
